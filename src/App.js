0e1d28894077f54bb366ea2450e262d0914ca6df
Subi <msubiantara@thebrandagency.co>
2022-12-21 17:26:17 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import validator from "validator";
 
 function App() {
+
+  const [signUpInput, setSignUpInput] = useState({
+    email: "",
+    password: "",
+    confirmPassword: ""
+  })
+
+  const [errorMessage, setErrorMessage] = useState("")
+
+  const handleChange = (e) => {
+    setSignUpInput({
+      ...signUpInput,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if(!validator.isEmail(signUpInput.email)){
+      return setErrorMessage("This email address is not valid")
+    } else {
+      if(signUpInput.password.length < 5) {
+        return setErrorMessage("The password you entered should contain 5 or more character")
+        } else if(!(signUpInput.password === signUpInput.confirmPassword)){
+          return setErrorMessage("The password don't match. Try again")
+        }
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container my-5">
+      <form>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className="form-control"
+            value={signUpInput.email}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="form-control"
+            value={signUpInput.password}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirm-password" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirm-password"
+            name="confirmPassword"
+            className="form-control"
+            value={signUpInput.confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+        {errorMessage && <p>{errorMessage}</p>}
+        <button  type="submit" className="btn btn-primary" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,105 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+beforeEach (() => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+})
+
+const typeIntoForm = ({ email, password, confirmPassword}) => {
+  const emailInpulEl = screen.getByLabelText("Email address");
+  const passwordInputEl = screen.getByLabelText("Password");
+  const confirmPasswordInputEl = screen.getByLabelText("Confirm Password");
+
+  if (email) {
+    userEvent.type(emailInpulEl, email)
+  } 
+  if (password) {
+    userEvent.type(passwordInputEl, password)
+  }
+  if (confirmPassword) {
+    userEvent.type(confirmPasswordInputEl, confirmPassword)
+  }
+  return {
+    emailInpulEl,
+    passwordInputEl,
+    confirmPasswordInputEl
+  }
+}
+
+const clickSubmitButton = () => {
+  const submitButtonEl = screen.getByRole("button", {name: "Submit"})
+  userEvent.click(submitButtonEl)
+}
+
+test('Login form input should be initially empty', () => {
+ 
+  expect(screen.getByLabelText("Email address")).toBeVisible()
+  expect(screen.getByLabelText("Password")).toBeVisible()
+  expect(screen.getByLabelText("Confirm Password")).toBeVisible()
+
+});
+
+test('User type in into email textbox', () => {
+
+  const {emailInpulEl} = typeIntoForm({email: "subi@gmail.com"})
+  expect(emailInpulEl.value).toBe("subi@gmail.com")
+
+});
+
+test('User type in into password textbox', () => {
+
+  const {passwordInputEl} = typeIntoForm({password: "password123"})
+  expect(passwordInputEl.value).toBe("password123")
+
+});
+
+test('User type in into confirm password textbox', () => {
+
+  const {confirmPasswordInputEl} = typeIntoForm({confirmPassword: "password123"})
+  expect(confirmPasswordInputEl.value).toBe("password123")
+
+});
+
+test('Display error message for invalid email address', () => {
+
+  expect(screen.queryByText(/This email address is not valid/i)).not.toBeInTheDocument()
+
+  typeIntoForm({email: "abcd"})
+  clickSubmitButton()
+
+  const errorMessageAfterSubmit = screen.getByText("This email address is not valid")
+  expect(errorMessageAfterSubmit).toBeVisible()
+
+});
+
+test('Display error message for password less than 5 characters', () => {
+
+
+  expect(screen.queryByText(/The password you entered should contain 5 or more character/i)).not.toBeInTheDocument()
+
+  typeIntoForm({
+    email: "subi@gmail.com",
+    password: "1234"
+  })
+  clickSubmitButton()
+
+  expect(screen.getByText("The password you entered should contain 5 or more character")).toBeVisible()
+
+});
+
+test('Display error message for incorrect confirm password', () => {
+  
+  expect(screen.queryByText(/The password don't match. Try again./i)).not.toBeInTheDocument()
+
+  typeIntoForm({
+    email: "subi@gmail.com",
+    password: "pass123",
+    confirmPassword: "pass1234"
+  })
+  clickSubmitButton()
+
+
+  expect(screen.getByText("The password don't match. Try again")).toBeVisible()
+
 });
